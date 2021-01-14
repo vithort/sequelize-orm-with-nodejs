9f30110a3fb2c00fb624226dab36e49f5dc3df8d
Vithor <vithort@gmail.com>
2021-01-14 14:07:02 -0300

    Insert Entities
    Insert Entities

--- create_model.js
+++ create_model.js
@@ -18,8 +18,45 @@ const Student = db.define('student', {
   },
 });
 
+const task = async () => {
+  try {
+    await db.sync({ alter: true });
+
+    // Insert a Student
+    /*
+    await Student.create({
+      name: 'Some Person',
+      age: 20,
+    });
+    */
+    for (let i = 0; i < 30; i++) {
+      await Student.create({
+        name: [
+          'Vithor',
+          'Cesar',
+          'Eros',
+          'Caimi',
+          'Jussara',
+          'Iara',
+          'Djara',
+          'Ubiratan',
+          'Ubirajara',
+          'Jurandy',
+        ][parseInt(Math.random() * 10)],
+        age: 10 + parseInt(Math.random() * 10),
+      });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+task();
+
 // alter: faz alterações
 // force: faz o drop e cria novamente
+/*
 db.sync({ alter: true })
   .then(() => console.log('Database synchronized!'))
   .catch(console.error);
+*/
